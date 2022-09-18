44add9ec65641b348947f371635b8d3deddd3d0e
sergey <sso96@mail.ru>
2022-09-18 19:39:08 +0300

    Testability

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int mils = 1;
-        int cashTicket = 20;
-        int allCash = 21;
-
-        int amountMils;
-
-        if (allCash >= cashTicket) {
-            amountMils = allCash / cashTicket * mils;
-        } else {
-            amountMils = 0;
-        }
-        System.out.println("Вам начислено " + amountMils + " миль(я)");
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
